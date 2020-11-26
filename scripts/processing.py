23e0b774b578361543bab3730b22f123eafcca63
vSebas <sebas.martp@gmail.com>
2020-11-26 01:05:41 -0600

    Angular velocity, linear acceleration and orientation working

--- scripts/processing.py
+++ scripts/processing.py
@@ -3,7 +3,6 @@
 import rospy
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import FluidPressure
-# from tf.transformations import euler_from_quaternion, quaternion_from_euler
 
 def get_height(msg,publisher_height):
     # Pabs = gauge + Patm
@@ -13,15 +12,6 @@ def get_height(msg,publisher_height):
 
     odom = Odometry()
     odom.pose.pose.position.z = -height
-    #  odom.pose.pose.position.x = 0
-    #  odom.pose.pose.position.y = 0
-    #  odom.twist.twist.linear.x = 0
-    #  odom.twist.twist.linear.y = 0
-    #  odom.twist.twist.linear.z = 0
-    #  odom.twist.twist.angular.x = 0
-    #  odom.twist.twist.angular.y = 0
-    #  odom.twist.twist.angular.z = 0
-    #  odom.header = msg.header
     odom.header.frame_id = "odom"
     odom.child_frame_id = "vtec_u3/base_link"
     odom.header.stamp = rospy.Time.now()
@@ -31,9 +21,8 @@ def get_height(msg,publisher_height):
 
 def processing():
     rospy.init_node('processing')
-    publisher_height = rospy.Publisher('/vtec_u3/odom_height', Odometry, queue_size=10) ######CAMBIAR
+    publisher_height = rospy.Publisher('/vtec_u3/odom_height', Odometry, queue_size=10)
     rospy.Subscriber ('/vtec_u3/pressure', FluidPressure, get_height, publisher_height)
-    # rate = rospy.Rate(20)
     rospy.spin()
 
 if __name__ == '__main__':
