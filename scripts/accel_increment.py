9ad0b80054d26ae1d8fc10e2272b757189878c60
vSebas <sebas.martp@gmail.com>
2021-04-11 12:31:52 -0500

    Imu message built, ready for tests

--- scripts/accel_increment.py
+++ scripts/accel_increment.py
@@ -15,31 +15,43 @@
 
 import rospy
 from nav_msgs.msg import Odometry
-from gazebo_msgs.msg import ModelStates, ModelState
-from sensor_msgs import Imu
- 
-def updateAccel(msg,publisher_accel_increment):
-  modelState = ModelState()
-  modelState.model_name = msg.name[2]
-  modelState.pose = msg.pose[2]
+from geometry_msgs.msg import Twist, Pose
+from gazebo_msgs.msg import ModelStates
+from sensor_msgs.msg import Imu
 
-  modelState.twist.linear.x = msg.twist[2].linear.x + 0.1
-  modelState.twist.linear.y = msg.twist[2].linear.y
-  modelState.twist.linear.z = msg.twist[2].linear.z
-  
-  modelState.twist.angular.x = msg.twist[2].angular.x
-  modelState.twist.angular.y = msg.twist[2].angular.y
-  modelState.twist.angular.z = msg.twist[2].angular.z
+class Imu_msg:
+  def __init__(self):
+    self.imu_msg = Imu()
+    rospy.init_node('pub_imu_accel')
+    self.pub_imu = rospy.Publisher('/vtec_u3/imu', Imu, queue_size= 1)
+    rospy.Subscriber('/uuv_accel', Twist, self.updateAccel)
+    # rospy.Subscriber('/uuv_simulation/dynamic_model/pose', Pose, updateAccel, pub_imu)
+    rospy.Subscriber('/uuv_simulation/dynamic_model/vel', Twist, self.updateVel)
+    rospy.Subscriber('/gazebo/model_states', ModelStates, self.updateAttitude)
 
-  rospy.loginfo(modelState)
-  publisher_accel_increment.publish(modelState)
+  def updateVel(self, msg):
+      self.imu_msg.angular_velocity.x = 0
+      self.imu_msg.angular_velocity.y = 0
+      self.imu_msg.angular_velocity.z = msg.angular.z
 
+  def updateAttitude(self, msg):
+      i = msg.name.index('vtec_u3')
+      self.imu_msg.orientation.x = msg.pose[i].orientation.y
+      self.imu_msg.orientation.y = msg.pose[i].orientation.x
+      self.imu_msg.orientation.z = -msg.pose[i].orientation.z
+      self.imu_msg.orientation.w = msg.pose[i].orientation.w
 
-def accel_increment():
-  rospy.init_node('accel_increment')
-  publisher_accel_increment = rospy.Publisher('/gazebo/set_model_state', ModelState, queue_size= 10)
-  rospy.Subscriber('/gazebo/model_states', ModelStates, updateAccel, publisher_accel_increment)
+  def updateAccel(self, msg):
+      self.imu_msg.linear_acceleration.x = msg.linear.x
+      self.imu_msg.linear_acceleration.y = msg.linear.y
+      self.imu_msg.linear_acceleration.z = msg.linear.z
+
+  def pubMsg(self):
+      self.pub_imu.publish(self.imu_msg)
 
 if __name__ == '__main__':
-  accel_increment()
-  rospy.spin()
+  imu = Imu_msg()
+  rate = rospy.Rate(10)
+  while(not rospy.is_shutdown()):
+    imu.pubMsg()
+    rate.sleep()
